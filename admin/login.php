091e8842aeac50b7df5865f4f409df2b44eb1969
Harshkumar Patel <hrspatel1111@gmail.com>
2024-02-05 23:13:44 +1100

    add code to check the login data

--- admin/login.php
+++ admin/login.php
@@ -46,7 +46,32 @@
     </div>
   </div></div>
 <br>
- 
+  <?php
+    include 'conn.php';
+
+  if(isset($_POST["login"])){
+
+    $username=mysqli_real_escape_string($conn,$_POST["username"]);
+    $password=mysqli_real_escape_string($conn,$_POST["password"]);
+
+    $sql="SELECT * from admin_info where admin_username='$username' and admin_password='$password'";
+    $result=mysqli_query($conn,$sql) or die("query failed.");
+
+    if(mysqli_num_rows($result)>0)
+    {
+      while($row=mysqli_fetch_assoc($result)){
+        session_start();
+         $_SESSION['loggedin'] = true;
+        $_SESSION["username"]=$username;
+        header("Location: dashboard.php");
+      }
+    }
+    else {
+      echo '<div class="alert alert-danger" style="font-weight:bold"> Username and Password are not matched!</div>';
+    }
+
+  }
+   ?>
  </form>
 </body>
 </html>
