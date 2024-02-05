c7139abf98cbbe4515a7e4a3e23cbfe7d19f52bd
Krish Moradiya <63234603+Krish7141@users.noreply.github.com>
2024-02-05 23:21:28 +1100

    why donate blood added

--- why_donate_blood.php
+++ why_donate_blood.php
@@ -1,5 +1,4 @@
 <html>
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,28 +14,43 @@
 <body>
 
 
+<?php 
+$active ='why';
+include('head.php');
+?>
 
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+  <div class="container">
+  <div id="content-wrap" style="padding-bottom:50px;">
+<div class="row">
+    <div class="col-lg-6">
+        <h1 class="mt-4 mb-3">Why Should I Donate Blood ? </h1>
+        <p>
+          <?php
+            include 'conn.php';
+            $sql=$sql= "select * from pages where page_type='donor'";
+            $result=mysqli_query($conn,$sql);
+            if(mysqli_num_rows($result)>0)   {
+                while($row = mysqli_fetch_assoc($result)) {
+                  echo $row['page_data'];
+                }
+              }
 
-  <div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
-    <div class="container">
-      <div id="content-wrap" style="padding-bottom:50px;">
-        <div class="row">
-          <div class="col-lg-6">
-            <h1 class="mt-4 mb-3">Why Should I Donate Blood ? </h1>
-
-
-          </div>
-          <div class="col-lg-6">
-            <img class="img-fluid rounded" src="image\08f2fccc45d2564f74ead4a6d5086871.png" style="height:600px; width:500px" alt="error">
-          </div>
-        </div>
-      </div>
+           ?>
+      </p>
 
     </div>
+    <div class="col-lg-6">
+        <img class="img-fluid rounded" src="image\08f2fccc45d2564f74ead4a6d5086871.png" style="height:600px; width:500px" alt="error"  >
+    </div>
+</div>
+</div>
 
-    <?php include('footer.php')
-    ?>
-  </div>
+</div>
+
+<?php include('footer.php')
+?>
+</div>
 </body>
 
 </html>
